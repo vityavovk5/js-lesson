bb1f28e6a56a880246bd87bba46f9bd29388afd7
vityavovk5 <vityavovk55@gmail.com>
2020-11-18 20:48:27 +0200

    done

---
+++ js/main.js
@@ -0,0 +1,48 @@
+// function firstFunction(){
+//     document.write("dsfsdf");
+// }
+
+// firstFunction();
+
+// function kvadrat(n) {
+//    return n * n;
+// }
+// let a =12;
+// document.write(kvadrat(a));
+
+// document.write(kvadrat(3));
+ 
+// let kvadrat = (n) => {
+//     return n * n;
+// }
+
+// function sum_kvad(n) {
+//    return [n + n, n * n];
+//  }
+//  let result = sum_kvad(12);
+//  document.write(result);
+
+
+// function sum_kvad(n) {
+//        return {
+//            sum: n + n,
+//            kvad: n + n
+//        }
+//      }
+//      let result = sum_kvad(12);
+//      document.write(result);
+
+
+ 
+// function sum_kvadrat (n) {
+//     //return n*n, n+n;
+//     return{
+//         sum: n+n,
+//         kvadrat: n*n
+//     }
+// }
+
+// let result = sum_kvadrat(12);
+// document.write(result);
+// console.log(result);
+// document.write(`${result.sum}, ${result.kvadrat}`);
